3f85e1b59edc10d346648df478e4e9f0813a0aa7
Irina Nicula <irina.nicula0@gmail.com>
2019-04-01 21:54:41 +0200

    Added methods for the search and to handle a term change in the input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,16 +4,28 @@ import './SearchBar.css';
 class SearchBar extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            term: 'jdjd'
+        }
+        this.search = this.search.bind(this);
+        this.handletermChange = this.handletermChange.bind(this);
     }
 
     search(){
-        
+        this.props.onSearch(this.state.term);
     }
+
+    handletermChange(event){
+        this.setState({
+            term: event.target.value
+        });
+    }
+    
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <a>SEARCH</a>
+                <input onChange = {this.handletermChange} placeholder="Enter A Song, Album, or Artist" />
+                <a onClick = {this.search}>SEARCH</a>
             </div>
         );
     }
